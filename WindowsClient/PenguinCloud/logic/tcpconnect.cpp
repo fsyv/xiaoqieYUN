4554a682e58899085e8f9edffbe315e5ef68b25e
liutimo <779564531@qq.com>
2017-03-31 00:10:22 +0800

    其实就是一个QTcpSocket，端口号是我自己测试程序的

---
+++ WindowsClient/PenguinCloud/logic/tcpconnect.cpp
@@ -0,0 +1,6 @@
+#include "tcpconnect.h"
+
+TcpConnect::TcpConnect()
+{
+
+}
---
+++ WindowsClient/PenguinCloud/logic/tcpconnect.h
@@ -0,0 +1,11 @@
+#ifndef TCPCONNECT_H
+#define TCPCONNECT_H
+
+
+class TcpConnect
+{
+public:
+    TcpConnect();
+};
+
+#endif // TCPCONNECT_H
