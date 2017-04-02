66146461666fa2c0e6eaa4c1c2aac832ffc001d7
fsyv <fsyv@qq.com>
2017-04-02 21:20:37 +0800

    登录

--- WindowsClient/PenguinCloud/loginwidget.cpp
+++ WindowsClient/PenguinCloud/loginwidget.cpp
@@ -7,15 +7,19 @@
 #include <QTcpSocket>
 #include "logic/rwfile.h"
 #include "network/connecttoserver.h"
+#include "mainwidget.h"
 
-LoginWidget::LoginWidget(QWidget *parent) : BasicWidget(parent)
+LoginWidget::LoginWidget(QWidget *parent) :
+    BasicWidget(parent),
+    m_pConnectToServer(nullptr),
+    m_pMainWidget(nullptr)
 {
-
+    
     this->setBackgroundColor("#0698f5");
     resize(320,400);
     init();
     setStyleSheetFromFile(":/resource/qss/login.qss");
-
+    
     RWFile rw(":/password.txt");
     QPair<QString, QString> info = rw.read();
     if(!info.first.isEmpty())
@@ -23,16 +27,23 @@ LoginWidget::LoginWidget(QWidget *parent) : BasicWidget(parent)
     if(!info.second.isEmpty())
         password_input->setText(info.second);
 
+    m_pConnectToServer = ConnectToServer::getInstance();
+
+    connect(m_pConnectToServer, SIGNAL(readyReadLoginMsg(LoginMsg)), this, SLOT(login_success(LoginMsg)));
 }
 
 LoginWidget::~LoginWidget()
 {
+    m_pConnectToServer = nullptr;
 
+    if(m_pMainWidget)
+        delete m_pMainWidget;
+    m_pMainWidget = nullptr;
 }
 
 void LoginWidget::init()
 {
-
+    
     label_title = new QLabel(this);
     QPixmap pix(":/resource/image/title.png");
     label_title->setPixmap(pix);
@@ -45,7 +56,7 @@ void LoginWidget::init()
     login_button->resize(200,30);
     remember_password = new QCheckBox("记住密码", this);
     find_passord = new QLabel("找回密码", this);
-
+    
     //绝对定位
     username_input->move((this->width() - username_input->width()) /2, 150);
     password_input->move((this->width() - password_input->width()) /2 ,183);
@@ -58,10 +69,10 @@ void LoginWidget::init()
     password_input->setPlaceholderText("密码");
     password_input->setEchoMode(QLineEdit::Password);
     login_button->setObjectName("login_button");
-
-
+    
+    
     connect(login_button, &QPushButton::clicked, this, &LoginWidget::conn);
-
+    
 }
 void LoginWidget::setStyleSheetFromFile(const QString &filename)
 {
@@ -74,39 +85,53 @@ void LoginWidget::setStyleSheetFromFile(const QString &filename)
 }
 void LoginWidget::conn()
 {
-   // sock = ConnectToServer::getInstance();
-    login_button->setText(tr("login..."));
-   // connect(sock, &ConnectToServer::readyRead, this, &LoginWidget::login_success);
-   // if(sock->state() != QAbstractSocket::UnconnectedState)
-    //{
-        QString data = username_input->text() + "@" + password_input->text();
+//    sock = ConnectToServer::getInstance();
+      login_button->setText(tr("login..."));
+//    connect(sock, &ConnectToServer::readyRead, this, &LoginWidget::login_success);
+//    if(sock->state() != QAbstractSocket::UnconnectedState)
+//    {
 //        Login *login = new Login();
 //        login->sendUserInfo(data);
+        
+//    }else
+//    {
+//        login_button->setText("Check NetWork");
+//        return;
+//    }
+//    connect(sock, &ConnectToServer::readyRead, this, &LoginWidget::login_success);
 
-  //  }else
-    //{
-       // login_button->setText("Check NetWork");
-      //  return;
-   // }
-   // connect(sock, &ConnectToServer::readyRead, this, &LoginWidget::login_success);
-
-        LoginMsg loginMsg;
-        memset(&loginMsg, 0, sizeof(LoginMsg));
-
-        loginMsg.m_iLoginStatus = PUT_LOGIN;
-        memcpy(loginMsg.m_aUserName, username_input->text().toLatin1().data(), username_input->text().length());
-        memcpy(loginMsg.m_aUserPass, password_input->text().toLatin1().data(), password_input->text().length());
-        ConnectToServer::getInstance()->sendLoginMsg(loginMsg);
+    
+    LoginMsg loginMsg;
+    memset(&loginMsg, 0, sizeof(LoginMsg));
+    
+    loginMsg.m_iLoginStatus = PUT_LOGIN;
+    memcpy(loginMsg.m_aUserName, username_input->text().toLatin1().data(), username_input->text().length());
+    memcpy(loginMsg.m_aUserPass, password_input->text().toLatin1().data(), password_input->text().length());
+    ConnectToServer::getInstance()->sendLoginMsg(loginMsg);
 }
 
-void LoginWidget::login_success()
+void LoginWidget::login_success(LoginMsg loginMsg)
 {
-    QString str = sock->readAll();
-    if(str != "")
+    qDebug() << "login_success";
+
+    if(loginMsg.m_iLoginStatus == LOGIN_SUCCESS)
     {
-        login_button->setText(tr("login success"));
-        sock->close();
+        hide();
+
+        if(m_pMainWidget)
+            delete m_pMainWidget;
+
+        m_pMainWidget = new MainWidget;
+
+        m_pMainWidget->show();
+    }
+    else if(loginMsg.m_iLoginStatus == LOGIN_FAILED_PASSWD)
+    {
+
+    }
+    else if(loginMsg.m_iLoginStatus == LOGIN_FAILED_USERNAME)
+    {
+
     }
 
-//    disconnect(sock, &ConnectToServer::readyRead, this, &LoginWidget::login_success);
 }
--- WindowsClient/PenguinCloud/loginwidget.h
+++ WindowsClient/PenguinCloud/loginwidget.h
@@ -12,6 +12,9 @@ class QComboBox;
 class QLabel;
 class QCheckBox;
 class QTcpSocket;
+class ConnectToServer;
+class MainWidget;
+
 class LoginWidget : public BasicWidget
 {
     Q_OBJECT
@@ -22,17 +25,19 @@ public:
 
 public slots:
     void conn();
-    void login_success();
+    void login_success(LoginMsg loginMsg);
 private:
     void init(); // 设置窗口属性以及变量的初始化
     void setStyleSheetFromFile(const QString &filename);
+
     QPushButton *login_button; // 登陆按钮
     LineEdit *password_input; // 密码输入框
     LineEdit *username_input; // 用户账号输入框
     QLabel *label_title;
     QCheckBox *remember_password;
     QLabel *find_passord;
-    QTcpSocket *sock ;
+    ConnectToServer *m_pConnectToServer;
+    MainWidget *m_pMainWidget;
 };
 
 #endif // LOGINWIDGET_H
