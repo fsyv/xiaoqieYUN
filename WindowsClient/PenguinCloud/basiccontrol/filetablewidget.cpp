5e977ae6e0106b7bf65508ae8da78fadadff84fd
liutimo <779564531@qq.com>
2017-04-03 11:38:07 +0800

    文件列表菜单

--- WindowsClient/PenguinCloud/basiccontrol/filetablewidget.cpp
+++ WindowsClient/PenguinCloud/basiccontrol/filetablewidget.cpp
@@ -2,6 +2,10 @@
 #include <QFileIconProvider>
 #include <QFileInfo>
 #include <QHeaderView>
+#include <QMouseEvent>
+#include <QMenu>
+#include <QAction>
+#include <QDebug>
 FileTableWidget::FileTableWidget(QWidget *parent) : QTableWidget(parent)
 {
 
@@ -47,8 +51,10 @@ void FileTableWidget::init()
 
 }
 
+//
 void FileTableWidget::setTableRow(QVector<QStringList> &_vec)
 {
+    //clear();
     for(int row = 0; row < _vec.size(); ++row)
     {
         QStringList elem = _vec[row];
@@ -58,7 +64,6 @@ void FileTableWidget::setTableRow(QVector<QStringList> &_vec)
             QTableWidgetItem *item;
             if( i == 0 )
             {
-
                 QFileIconProvider p;
                 QFileInfo fileinfo(elem[0]);
                 QIcon icon = p.icon(fileinfo);
@@ -72,4 +77,92 @@ void FileTableWidget::setTableRow(QVector<QStringList> &_vec)
         }
 
     }
+}
+
+
+void FileTableWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::RightButton)
+    {
+//        删除 下载  移动  复制  分享
+        QMenu *item_menu = new QMenu();
+        QAction *download_action = new QAction(tr("下载"));
+        QAction *delete_action = new QAction(tr("删除"));
+        QAction *rename_action = new QAction(tr("重命名"));
+        QAction *copy_action = new QAction(tr("复制"));
+        QAction *move_action = new QAction(tr("移动"));
+        QAction *share_action = new QAction(tr("分享"));
+
+        connect(rename_action, &QAction::triggered, this, &FileTableWidget::rename);
+        connect(delete_action, &QAction::triggered, this, &FileTableWidget::del);
+        connect(download_action, &QAction::triggered, this, &FileTableWidget::download);
+        connect(copy_action, &QAction::triggered, this, &FileTableWidget::copy);
+        connect(move_action, &QAction::triggered, this, &FileTableWidget::moveitem);
+        connect(share_action, &QAction::triggered, this, &FileTableWidget::share);
+
+
+
+
+        item_menu->addAction(download_action);
+        item_menu->addAction(delete_action);
+        item_menu->addAction(rename_action);
+        item_menu->addAction(copy_action);
+        item_menu->addAction(move_action);
+        item_menu->addAction(share_action);
+
+        //新建文件夹   上传
+        QMenu *noitem_menu = new QMenu();
+        QAction *new_folder_action = new QAction(tr("新建文件夹"));
+        QAction *upload_action = new QAction(tr("上传"));
+
+        noitem_menu->addAction(new_folder_action);
+        noitem_menu->addAction(upload_action);
+
+//        记录菜单显示的位置
+        menu_show = event->pos();
+        if( itemAt(event->pos()) != 0)
+             item_menu->exec(QCursor::pos());
+        else
+            noitem_menu->exec(QCursor::pos());
+
+    }
+}
+
+
+void FileTableWidget::rename()
+{
+    QTableWidgetItem *item = itemAt(menu_show);
+    setCurrentCell(item->row(), 0);
+    openPersistentEditor(item);
+    editItem(item);
+
+    //
+}
+
+void FileTableWidget::del()
+{
+    QTableWidgetItem *item = itemAt(menu_show);
+    this->removeRow(item->row());
+
+    //
+}
+
+void FileTableWidget::download()
+{
+    qDebug() << "DownLoad";
+}
+
+void FileTableWidget::share()
+{
+    qDebug() << "share";
+}
+
+void FileTableWidget::moveitem()
+{
+    qDebug() << "move";
+}
+
+void FileTableWidget::copy()
+{
+    qDebug() << "copy";
 }
--- WindowsClient/PenguinCloud/basiccontrol/filetablewidget.h
+++ WindowsClient/PenguinCloud/basiccontrol/filetablewidget.h
@@ -10,12 +10,20 @@ public:
     FileTableWidget(QWidget *parent = nullptr);
     void setTableRow(QVector<QStringList> &_vec);
 public slots:
+    void rename();
+    void del();
+    void download();
+    void share();
+    void moveitem();
+    void copy();
 
+protected:
+    void mouseReleaseEvent(QMouseEvent *event);
 private:
     void init();
 
     QVector<QStringList> files;
-
+    QPoint menu_show; // 记录菜单显示的位置
 };
 
 #endif // FILETABLEWIDGET_H
