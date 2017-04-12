046797a7a39e49a02a1cf4c71f1a1a0e9477ef60
liutimo <779564531@qq.com>
2017-04-12 23:53:23 +0800

    新增

---
+++ WindowsClient/PenguinCloud/basiccontrol/managewidget.cpp
@@ -0,0 +1,36 @@
+﻿#include "managewidget.h"
+
+ManageWidget::ManageWidget(QWidget *parent) : BasicWidget(parent)
+{
+    resize(800, 600);
+    init();
+}
+
+ManageWidget::~ManageWidget()
+{
+    delete download_manage;
+    delete upload_manage;
+    delete tabwidget;
+}
+
+void ManageWidget::init()
+{
+    tabwidget = new QTabWidget(this);
+    tabwidget->resize(this->size());
+
+    download_manage = new DownloadManage(this);
+    upload_manage = new UploadManage(this);
+
+    tabwidget->addTab(download_manage, tr("下载管理"));
+    tabwidget->addTab(upload_manage, tr("上传管理"));
+}
+
+DownloadManage *ManageWidget::getDownloadManage()
+{
+    return download_manage;
+}
+
+UploadManage *ManageWidget::getUploadManage()
+{
+    return upload_manage;
+}
---
+++ WindowsClient/PenguinCloud/basiccontrol/uploadmanage.h
@@ -0,0 +1,20 @@
+﻿#ifndef UPLOADMANAGE_H
+#define UPLOADMANAGE_H
+
+#include <QtWidgets>
+
+#include "basicwidget/basicwidget.h"
+
+class UploadManage : public BasicWidget
+{
+public:
+    UploadManage(QWidget *parent = 0);
+    void addRow(const QString &filename, QProgressBar* &progress, QPushButton* &cancel, QPushButton* &paste);
+    ~UploadManage();
+
+private:
+    void init();
+    QTableWidget *upload;
+};
+
+#endif // UPLOADMANAGE_H
